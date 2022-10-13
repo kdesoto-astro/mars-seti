0d73a68b60e10f1a3a341704180cba8c8742cfb6
Kaylee de Soto <kmd6851@psu.edu>
2022-10-13 16:32:00 -0400

    Begin XML signal parsing

--- track_signals.py
+++ track_signals.py
@@ -1,7 +1,28 @@
-import urllib
+import urllib.request
 
 DSN_URL = "https://eyes.nasa.gov/dsn/data/dsn.xml"
+VALID_STATIONS = ["MVN", "MEX"]
 
+def parse_signal_xml(raw_string, valid_disks):
+    """
+    Parses the chunk of XML data to return signal
+    objects for a given station.
+    """
+    parse_dish = raw_string.split("</dish>")
+    
+    parsed_valid_dishes = []
+    for d in parse_dish:
+        parse_dish2 = d.split("<dish name=")
+        
+        for d2 in parse_dish2:
+            if len(d2) < 10 or d2[1:4] != "DSS":
+                continue
+            print(d2[1:6])
+            if int(d2[4:6]) in valid_disks:
+                parsed_valid_dishes.append(d2)
+    
+    print(parsed_valid_dishes)
+    
 class Signal:
     def __init__(self, t, freq, power, spacecraft):
         """
@@ -13,7 +34,7 @@ class Signal:
         self.spacecraft = spacecraft
     
 class Station:
-    def __init__(self, name):
+    def __init__(self, name, disk_list):
         """
         Initializes the Station object, which tracks signal
         information for one of the three DSN stations (Canberra, 
@@ -22,6 +43,7 @@ class Station:
         self.name = name
         self.uplink_list = []
         self.downlink_list = []
+        self.disks = disk_list
         
     def add_uplink(self, link_object):
         """
@@ -44,23 +66,44 @@ class Station:
         (prev_downlink.freq != link_object.freq) or \
         (prev_downlink.power != link_object.power):
             self.downlink_list.append(link_object)
+            
+    def fetch_signals(self):
+        """
+        Fetches the current uplink/downlink status from the
+        Deep Space Network stations. Adds new uplinks/downlinks as
+        needed.
+        """
+        req = urllib.request.Request(DSN_URL)
+        resp = urllib.request.urlopen(req)
+        data = resp.read().decode("utf-8") 
+        
+        signals = parse_signal_xml(data, self.disks)
+        
+        print(signals)
+    
+    def log_signals(self):
+        """
+        Offloads signal lists to file for more
+        permanent storage.
+        """
+        pass
         
 class CanberraStation(Station):
     def __init__(self):
-        super().__init__(self, "Canberra")
+        super().__init__("Canberra", [43, 34, 35, 36])
         
 class GoldstoneStation(Station):
     def __init__(self):
-        super().__init__(self, "Goldstone")
+        super().__init__("Goldstone", [14, 24, 25, 26])
 
 class MadridStation(Station):
     def __init__(self):
-        super().__init__(self, "Madrid")
+        super().__init__("Madrid", [63, 65, 53, 54, 55, 56])
         
-        
-def fetch_signals():
-    """
-    Fetches the current uplink/downlink status from the
-    Deep Space Network stations.
-    """
-    pass
+def main():
+    canberra_stream = CanberraStation()
+    canberra_stream.fetch_signals()
+
+main()
+    
+    
